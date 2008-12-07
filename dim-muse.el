b3daf44df6492f82e452ad45963b5cbcc186b345
Dimitri Fontaine <dim@tapoueh.org>
2008-12-07 13:53:58 +0100

    CSS integration of the blog

--- dim-muse.el
+++ dim-muse.el
@@ -8,9 +8,15 @@
 
 (muse-derive-style
  "tapoueh-html" "html"
- :header "../../css/header.html"
- :footer "../../css/footer.html"
+ :header "../css/header.html"
+ :footer "../css/footer.html"
  :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"../../css/styles.css\" />") 
+
+(muse-derive-style
+ "tapoueh-journal-html" "journal-html"
+ :header "../css/blog-header.html"
+ :footer "../css/blog-footer.html"
+ :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"../css/styles.css\" />") 
 
 (setq muse-project-alist
       '(("pgsql.tapoueh.org" 
@@ -20,7 +26,7 @@
 		:path "~/dev/muse/out/site"))
 	
 	("blog.tapoueh.org" ("~/dev/muse/blog")
-	 (:base "journal-html" 
+	 (:base "tapoueh-journal-html" 
 		:path "~/dev/muse/out/blog")
 	 (:base "journal-rss" 
 		:base-url "http://blog/tapoueh.org/" 
