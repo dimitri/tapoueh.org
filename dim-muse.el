81bcea8fa6c54c003d04a4204632e8ed3cf8c29f
Dimitri Fontaine <dim@tapoueh.org>
2009-01-21 15:17:28 +0100

    File location reorganisation

--- dim-muse.el
+++ dim-muse.el
@@ -8,27 +8,27 @@
 
 (muse-derive-style
  "tapoueh-html" "html"
- :header "../css/header.html"
- :footer "../css/footer.html"
- :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"../../css/styles.css\" />") 
+ :header "~dim/dev/muse/css/header.html"
+ :footer "~dim/dev/muse/css/footer.html"
+ :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"css/styles.css\" />") 
 
 (muse-derive-style
  "tapoueh-journal-html" "journal-html"
- :header "../css/blog-header.html"
- :footer "../css/blog-footer.html"
- :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"../css/styles.css\" />"
+ :header "~dim/dev/muse/css/blog-header.html"
+ :footer "~dim/dev/muse/css/blog-footer.html"
+ :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"css/styles.css\" />"
  :date-format "%a, %e %b %Y, %k:%M")
 
 (setq muse-project-alist
       '(("pgsql.tapoueh.org" ("~/dev/muse/site" :default "index")
 	 (:base "tapoueh-html" 
-		:path "~/dev/muse/out/site"))
+		:path "~/dev/muse/out/"))
 	
 	("blog.tapoueh.org" ("~/dev/muse/blog")
 	 (:base "tapoueh-journal-html" 
-		:path "~/dev/muse/out/blog")
+		:path "~/dev/muse/out/")
 	 (:base "journal-rss" 
 		:base-url "http://blog.tapoueh.org/" 
-		:path "~/dev/muse/out/blog"))))
+		:path "~/dev/muse/out/"))))
 
 (provide 'dim-muse)
