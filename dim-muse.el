22920f0d50947952ac5a50ba99a47372c9ace817
Dimitri Fontaine <dim@tapoueh.org>
2009-02-02 23:01:08 +0100

    Trying tio produce PDFs

--- dim-muse.el
+++ dim-muse.el
@@ -4,8 +4,18 @@
 (require 'muse-html) 
 (require 'muse-journal)
 (require 'muse-latex)
+(require 'muse-context)
 
 (require 'muse-project)  ; publication par projets
+
+;; toolset paths
+(when (string-match "apple-darwin" system-configuration)
+  (setenv "PATH" (concat (getenv "PATH") ":/sw/bin"))
+  (setenv "TEXINPUTS" 
+	  (concat (getenv "TEXINPUTS") 
+		  "."
+		  ":/usr/local/texlive/2007/texmf-dist//" 
+		  ":/sw/share/texmf-dist//")))
 
 (muse-derive-style
  "tapoueh-html" "html"
@@ -20,12 +30,16 @@
  :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"css/styles.css\" />"
  :date-format "%a, %e %b %Y, %k:%M")
 
+(muse-derive-style
+ "tapoueh-pdf" "pdf"
+ :header "~dim/dev/muse/css/latex-header.tex")
+
 (setq muse-project-alist
       '(("pgsql.tapoueh.org" ("~/dev/muse/site" :default "index")
 	 (:base "tapoueh-html" 
 		:path "~/dev/muse/out/")
 
-	 (:base "pdf"
+	 (:base "tapoueh-pdf"
 		:path "~/dev/muse/pdf/"))
 	
 	("blog.tapoueh.org" ("~/dev/muse/blog")
