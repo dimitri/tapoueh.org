6af96976e1159e127e3c0b355a6fce596af08a33
Dimitri Fontaine <dim@tapoueh.org>
2010-03-02 18:40:16 +0100

    Implement a :final function to publish a separate page per article.

--- dim-muse.el
+++ dim-muse.el
@@ -31,7 +31,8 @@
  :footer "~dim/dev/tapoueh.org/css/footer.html"
  :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"css/styles.css\" />"
  :date-format "%a, %e %b %Y, %k:%M"
- :date-format-notime "%a, %e %b %Y")
+ :date-format-notime "%a, %e %b %Y"
+ :final 'tapoueh-journal-html-split-entries)
 
 (muse-derive-style
  "tapoueh-pdf" "pdf"
@@ -52,6 +53,70 @@
 	 (:base "journal-rss" 
 		:base-url "http://blog.tapoueh.org/" 
 		:path "~/dev/tapoueh.org/out/"))))
+
+(defun dim:muse-get-header-and-start ()
+  "return the header string and its end position"
+  (goto-char (point-min))
+  (re-search-forward "<div class=\"entry\">")
+  (beginning-of-line)
+  (cons (buffer-substring-no-properties 1 (point)) (point)))
+
+(defun dim:muse-get-footer-and-end ()
+  "return the footer string and its beginning position"
+  (goto-char (point-max))
+  (re-search-backward "<div id=\"footer\">")
+  (forward-line -2)
+  (cons (buffer-substring-no-properties (point) (point-max)) (point)))
+
+(defun dim:muse-collect-entries (start end)
+  "return a list of region positions where to find entries"
+  (goto-char start)
+  (forward-line -1)
+  (let ((positions '())
+	(name              "")
+	(date              "")
+	(current-position  (point))
+	(previous-position (point)))
+    (while (re-search-forward "<div class=\"entry\">" end t 2)
+      (forward-line -1)
+      (setq current-position (point))
+
+      ;; get the name too
+      (goto-char previous-position)
+      (let* ((hs (re-search-forward "<h2>"))
+	     (he (re-search-forward "</h2>"))
+	     (an (buffer-substring-no-properties hs (- he 5))))
+	(setq name (mapconcat 'identity (split-string an "[ /\\]") "_")))
+
+      (setq positions 
+	    (cons (list name previous-position current-position) positions))
+      (setq previous-position current-position)
+      (goto-char current-position))
+    positions))
+
+(defun tapoueh-journal-html-split-entries (source target sstarget)
+  "Split HTML output in one file per entry, called as a Muse :final function"
+  (save-excursion
+    (let* ((header-and-start (dim:muse-get-header-and-start))
+	   (header           (car header-and-start))
+	   (start            (cdr header-and-start))
+	   (footer-and-end   (dim:muse-get-footer-and-end))
+	   (footer           (car footer-and-end))
+	   (end              (cdr footer-and-end))
+	   (directory 
+	    (file-name-as-directory
+	     (concat 
+	      (file-name-directory target) 
+	      "articles/"
+	      (car (split-string (file-name-nondirectory target) "[.]"))))))
+      (dolist (e (dim:muse-collect-entries start end))
+	(let ((name (concat directory (car e) ".html"))
+	      (from (cadr e))
+	      (to   (caddr e)))
+	  (write-region header nil name)
+	  (write-region from to name 'append)
+	  (write-region footer nil name 'append)
+	  (message "tapoueh-journal-html-split-entries: %s." name))))))
 
 (defvar dim:muse-rsync-options "-avz"
   "rsync options")
