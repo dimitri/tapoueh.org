50bc9eaaa7bfa3b0be42a5c54b17ebe0fd3165f5
Dimitri Fontaine <dim@tapoueh.org>
2011-07-04 16:32:25 +0200

    M-x delete-trailing-whitespace

--- dim-muse.el
+++ dim-muse.el
@@ -1,7 +1,7 @@
 ;;; dim-muse.el --- Emacs Muse setup to publish my projects
 
 (require 'muse-mode)
-(require 'muse-html) 
+(require 'muse-html)
 (require 'muse-journal)
 (require 'muse-latex)
 (require 'muse-context)
@@ -13,17 +13,17 @@
 ;; toolset paths
 (when (string-match "apple-darwin" system-configuration)
   (setenv "PATH" (concat (getenv "PATH") ":/sw/bin"))
-  (setenv "TEXINPUTS" 
-	  (concat (getenv "TEXINPUTS") 
+  (setenv "TEXINPUTS"
+	  (concat (getenv "TEXINPUTS")
 		  "."
-		  ":/usr/local/texlive/2007/texmf-dist//" 
+		  ":/usr/local/texlive/2007/texmf-dist//"
 		  ":/sw/share/texmf-dist//")))
 
 (muse-derive-style
  "tapoueh-html" "html"
  :header "~/dev/tapoueh.org/css/header.html"
  :footer "~/dev/tapoueh.org/css/footer.html"
- :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"css/styles.css\" />") 
+ :style-sheet "<link rel=\"stylesheet\" type=\"text/css\"  media=\"all\" href=\"css/styles.css\" />")
 
 (muse-derive-style
  "tapoueh-journal-html" "journal-html"
@@ -41,18 +41,18 @@
 
 (setq muse-project-alist
       '(("pgsql.tapoueh.org" ("~/dev/tapoueh.org/site" :default "index")
-	 (:base "tapoueh-html" 
+	 (:base "tapoueh-html"
 		:path "~/dev/tapoueh.org/out/")
 
 	 (:base "pdf"
 	 	:path "~/dev/tapoueh.org/pdf/"))
-	
+
 	("blog.tapoueh.org" ("~/dev/tapoueh.org/blog")
-	 (:base "tapoueh-journal-html" 
+	 (:base "tapoueh-journal-html"
 		:path "~/dev/tapoueh.org/out/")
 
-	 (:base "journal-rss" 
-		:base-url "http://blog.tapoueh.org/" 
+	 (:base "journal-rss"
+		:base-url "http://blog.tapoueh.org/"
 		:path "~/dev/tapoueh.org/out/"))))
 
 ;;;
@@ -104,10 +104,10 @@
 	     (he (re-search-forward "</h2>"))
 	     (an (buffer-substring-no-properties hs (- he 5))))
 	(setq name
-	      (cons an (mapconcat 'identity 
+	      (cons an (mapconcat 'identity
 				  (split-string an "[ /\\?]\\|&amp;") "_"))))
 
-      (setq positions 
+      (setq positions
 	    (cons (list name previous-position current-position) positions))
       (setq previous-position current-position)
       (goto-char current-position))
@@ -123,7 +123,7 @@
 	  (dotimes (d (- depth 1)) (setq up (concat up "../")))
 
 	  (while (string-match "href=\"\\|src=\"" header pos)
-	    (setq 
+	    (setq
 	     relocated (concat relocated (substring header pos (match-end 0))))
 	    (setq pos (match-end 0))
 	    ;; get the link
@@ -142,7 +142,7 @@
 (defun dim:muse-make-index (articles &optional subdir n)
   "return the index content (string) from the article list, header and footer"
   (concat "<ul>\n"
-	  (mapconcat 
+	  (mapconcat
 	   (lambda (article)
 	     (let ((name  (caar  article))
 		   (fname (cdar  article))
@@ -153,25 +153,25 @@
 	       (let* ((ds (re-search-forward "<span class=\"date\">"))
 		      (de (re-search-forward "</span>"))
 		      (ad (buffer-substring-no-properties ds (- de 7))))
-		 (format 
-		  "<li><a href=\"%s.html\">%s</a>, %s</li>\n" 
+		 (format
+		  "<li><a href=\"%s.html\">%s</a>, %s</li>\n"
 		  (if subdir
 		      (concat (file-name-as-directory subdir) fname)
 		    fname)
 		  name ad))))
-	   (if n 
+	   (if n
 	       (cdr (butlast articles (- (length articles) n 1)))
-	     articles) 
+	     articles)
 	   "\n")
 	  "</ul>\n"))
 
 (defun tapoueh-journal-html-index ()
   ":after function for Muse, to add in the output buffer an articles index"
-  (let* ((subdir 
-	  (concat 
-	   "articles/" 
+  (let* ((subdir
+	  (concat
+	   "articles/"
 	   (car (split-string
-		 (file-name-nondirectory 
+		 (file-name-nondirectory
 		  muse-publishing-current-output-path) "[.]"))))
 	 (header-and-start (dim:muse-get-header-and-start))
 	 (header           (car header-and-start))
@@ -180,15 +180,15 @@
 	 (footer           (car footer-and-end))
 	 (end              (cdr footer-and-end))
 	 (articles         (dim:muse-collect-entries start end))
-	 (index            (dim:muse-make-index 
+	 (index            (dim:muse-make-index
 			    articles subdir dim:muse-index-length)))
     (goto-char start)
-    (insert 
+    (insert
      (concat
       "<div class=\"toc\"><h2>Previous Articles</h2>\n"
       index
       "\n"
-      (format 
+      (format
        "There's also an <a href=\"%s/index.html\">index of all entries</a>."
        subdir)
       "</div>\n\n"))))
@@ -206,9 +206,9 @@
 	   (articles         (dim:muse-collect-entries start end))
 	   (index            (dim:muse-make-index articles))
 	   (subdir
-	    (concat "articles/" 
+	    (concat "articles/"
 		    (car (split-string (file-name-nondirectory target) "[.]"))))
-	   (dir 
+	   (dir
 	    (file-name-as-directory
 	     (concat (file-name-directory target) subdir))))
 
@@ -221,12 +221,12 @@
 	      (from    (cadr a))
 	      (to      (caddr a)))
 	  (write-region header nil name)
-	  (write-region 
+	  (write-region
 	   (dim:muse-relocate-links
 	    (buffer-substring-no-properties from to) 2) nil name 'append)
 
 	  ;; add a local index
-	  (write-region 
+	  (write-region
 	   (concat "<div class=\"entry\"><h2>Index of All Articles</h2>\n"
 		   index
 		   "</div>\n\n") nil name 'append)
@@ -254,7 +254,7 @@
 (defun dim:muse-project-rsync (&optional static)
   "publish tapoueh.org using rsync"
   (interactive "P")
-  (let* ((rsync-command (format "rsync %s %s %s" 
+  (let* ((rsync-command (format "rsync %s %s %s"
 				dim:muse-rsync-options
 				(concat dim:muse-rsync-source "/")
 				(concat dim:muse-rsync-target "/"))))
@@ -267,7 +267,7 @@
 
       (when static
 	(dolist (subdir dim:muse-rsync-extra-subdirs)
-	  (let ((cmd (format "rsync %s %s %s" 
+	  (let ((cmd (format "rsync %s %s %s"
 			     dim:muse-rsync-options
 			     (concat dim:muse-rsync-source "/" subdir)
 			     dim:muse-rsync-target)))
