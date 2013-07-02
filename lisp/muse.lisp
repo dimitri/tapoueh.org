7cbc0da1a08b311d74290de0eb930041eedac525
Dimitri Fontaine <dim@tapoueh.org>
2013-07-02 14:22:23 +0200

    Another place where to downcase tags links...

--- lisp/muse.lisp
+++ lisp/muse.lisp
@@ -157,7 +157,7 @@
   "Format muse-tags to be displayed on the web"
   (loop
      for (tag . more?) on (muse-tags m)
-     collect `(:a :href (format nil "/tags/~a" ,tag) ,tag)
+     collect `(:a :href (format nil "/tags/~a" ,(string-downcase tag)) ,tag)
      when more? collect ", "))
 
 (defmethod muse-extract-article-image-source ((article muse))
